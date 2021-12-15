388507bba95b798017a53bf18f3252c7ffc69d80
Eldridge Turambi <eturambi95@gmail.com>
2021-12-14 21:24:39 -0700

    Update Status

--- spec/features/applicants/show_spec.rb
+++ spec/features/applicants/show_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "Show page of applicants", type: :feature do
   before :each do
     @shelter1 = Shelter.create!(foster_program: true, name: 'Love Shelter', city: 'Parker', rank: 1)
 
-    @applicant1 = Applicant.create!(name: 'Richie', street_address: "3707 south waco street", city: 'Aurora', state: "Colorado", zip_code: 80013, description: "blah", status: "blah blah")
+    @applicant1 = Applicant.create!(name: 'Richie', street_address: "3707 south waco street", city: 'Aurora', state: "Colorado", zip_code: 80013, description: "blah", status: "In Progress")
 
     @pet1 = @applicant1.pets.create!(adoptable: true, age: 4, breed: 'husky', name: 'Fido', shelter_id: @shelter1.id)
 
@@ -26,5 +26,6 @@ RSpec.describe "Show page of applicants", type: :feature do
     expect(page).to have_content(@applicant1.status)
 
     expect(page).to have_content(@pet1.name)
+
   end
 end
